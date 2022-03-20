a1bb943d99b15f9a2ffae271225cd3d73aa53033
Anupam Dahal <anupamdahal1@gmail.com>
2022-03-19 23:14:59 -0500

    Communication complete between client and server

--- servers/python/build-proto.sh
+++ servers/python/build-proto.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 PROTOS_DIR=../../protos
 GRPC_DIR=./src
+OUT_DIR=src/protos
 
-mkdir -p src/protos
+mkdir -p ${OUT_DIR}
 
 python3 -m grpc_tools.protoc \
   --python_out=${GRPC_DIR} \
   --grpc_python_out=${GRPC_DIR} \
   -I ../../ \
   ${PROTOS_DIR}/*.proto
+
+test -f ${OUT_DIR}/__init__.py || touch ${OUT_DIR}/__init__.py
